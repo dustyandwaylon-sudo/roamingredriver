76c761ae8d2aa14c3bd9a816051e47d9006a7ede
dustyandwaylon-sudo <dustyandwaylon@gmail.com>
2026-06-24 01:01:22 -0400

    feat: integrate stretch effect shader and add backend schema support for trails and venues

--- lib/pages/trail_detail/trail_detail_widget.dart
+++ lib/pages/trail_detail/trail_detail_widget.dart
@@ -699,7 +699,7 @@ class _TrailDetailWidgetState extends State<TrailDetailWidget> {
                                     ),
                                     onPressed: () async {
                                       FFAppState().isOfflineMode =
-                                          !(FFAppState().isOfflineMode ?? true);
+                                          !FFAppState().isOfflineMode;
                                       FFAppState().update(() {});
                                     },
                                   ),
